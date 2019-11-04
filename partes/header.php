6f3444602396b06f31bcb082aa34f0005c1e5f25
Josep129 <44200944+Josep129@users.noreply.github.com>
2019-11-04 13:56:06 -0500

    estlios

---
+++ partes/header.php
@@ -0,0 +1,14 @@
+<body>
+    <header>
+        <img src="imagenes/logo.jpg" alt="">
+    </header>
+    <nav>
+        <aside></aside>
+        <a href="Index.php">Inicio</a>
+        <a href="Foros.php">Foros</a>
+        <a href="Noticias.php">Noticias</a>
+        <a href="Videos.php">Videos</a>
+        <a href="Comunidad.php">Comunidad</a>
+        <aside></aside>
+    </nav>
+</body>
