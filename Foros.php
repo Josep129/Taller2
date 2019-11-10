6e2b4435e57bf1b02c2c28eeae211dea14a5e7e7
Josep129 <44200944+Josep129@users.noreply.github.com>
2019-11-10 09:30:13 -0500

    color

--- Foros.php
+++ Foros.php
@@ -11,7 +11,14 @@
     <div class="contenedor">
         <?php include'partes/header.php' ?>
         <aside></aside>
-        <article>article</article>
+        <article>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur consectetur consequatur sit sequi earum sapiente facere? Nulla cumque pariatur eaque voluptatibus reprehenderit, tempora inventore eveniet totam sapiente vitae quos? Accusamus.
+            In officiis facere atque neque culpa modi quae nulla, nostrum blanditiis eos natus placeat fuga mollitia assumenda impedit eaque similique veniam illum! At corrupti fugit aliquam nulla molestiae dignissimos nemo.
+            Consequuntur enim culpa harum obcaecati suscipit, ipsa placeat quos beatae iure nisi, laborum eum illum ut eligendi eos vel amet a repellat dolorum inventore quo voluptate voluptatem tempore? Alias, a!
+            Nobis consequuntur sunt architecto vitae consectetur laboriosam, iure quia omnis voluptatum, vel animi voluptates magni eaque explicabo ad blanditiis consequatur illum doloremque placeat. Enim libero iure numquam blanditiis! Quisquam, id?
+            Quae excepturi nulla repellendus expedita itaque ad corrupti impedit asperiores, numquam voluptate dolorum neque dolores aut cumque porro. Dolores nisi fuga distinctio eos culpa eius tempore enim corrupti nihil quos?
+            Corporis maiores ut dolorem animi velit nihil aperiam dolores distinctio! Dolorem odio, magnam perspiciatis impedit unde quae, sed ad voluptas pariatur illum distinctio eos provident veritatis! Earum soluta atque repellat.
+        </article>
         <aside></aside>
         <?php include'partes/footer.php' ?>
     </div>
